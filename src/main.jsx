9179627bb00ac3753e673e541700be6c97877f40
sachit <sachitforsocials@gmail.com>
2024-09-02 11:25:03 +0530

    add about us change in main.jsx change in foote.jsx change in Header.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+ 
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
+import About from './components/AboutUs/About.jsx'
 const router = createBrowserRouter([
   {
     path:"/",
