6de65913c605563b6559ce76c4db3bd375bd86cd
sachit <sachitforsocials@gmail.com>
2024-09-02 11:39:46 +0530

    add contact component update contact on header write new route design

--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
  
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/AboutUs/About.jsx'
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element: <Layout/>,
+import Contact from './components/Contact/Contact.jsx'
+// const router = createBrowserRouter([
+//   {
+//     path:"/",
+//     element: <Layout/>,
 
-    children:[
-      {
-        path:"",
-        element:<Home/>
+//     children:[
+//       {
+//         path:"",
+//         element:<Home/>
 
-      },
-      {
-        path:"about",
-        element:<About/>
-      }
-    ]
-  }
-])
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <Route path='/' element={<Layout/>}>
+        
+      <Route path="" element={<Home/>} />
+      <Route path='about' element={<About/>} />
+      <Route path = "contact" element={<Contact/>} />
+
+    </Route>
+  )
+)
 
 
 createRoot(document.getElementById('root')).render(
